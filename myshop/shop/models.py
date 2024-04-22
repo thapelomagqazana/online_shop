6a3977268c5859dc9349babe00c56a0cf489b98c
Thapelo Magqazana <tapsmcgzee8@gmail.com>
2024-04-22 13:39:37 +0200

    Created product catalog models

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    """Model representing a product category."""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the category name.")
+    slug = models.SlugField(max_length=200,
+                            unique=True,
+                            help_text="Unique URL path to access this category.")
+    
+    class Meta:
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        """String for representing the Category object."""
+        return self.name
+
+
+class Product(models.Model):
+    """Model representing a product."""
+    category = models.ForeignKey(Category,
+                                 related_name="products",
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, 
+                            help_text="Enter the product name.")
+    slug = models.SlugField(max_length=200, 
+                            help_text="Unique URL path to access this product.")
+    image = models.ImageField(upload_to="products/%Y/%m/%d",
+                              blank=True,
+                              help_text="Product image.")
+    description = models.TextField(blank=True,
+                                   help_text="Description of the product.")
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                help_text="Price of the product.")
+    available = models.BooleanField(default=True, 
+                                    help_text="Indicates whether the product is available for purchase.")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["id", "slug"]),
+            models.Index(fields=["name"]),
+            models.Index(fields=["-created"]),
+        ]
+
+    def __str__(self):
+        """String for representing the Product object."""
+        return self.name
