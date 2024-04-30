34ff5db6ae81a33b1bbd6a1943e2b2e8943863ed
Thapelo Magqazana <tapsmcgzee8@gmail.com>
2024-04-30 12:42:05 +0200

    Added a functionality that updates product quantities in the cart

--- myshop/cart/tests.py
+++ myshop/cart/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+# from django.test import TestCase, RequestFactory
+# from django.conf import settings
+# from django.urls import reverse
+# from shop.models import Product, Category
+# from .cart import Cart
+# from .views import cart_add, cart_remove, cart_detail
+# from .forms import CartAddProductForm
 
-# Create your tests here.
+# # Create your tests here.
+# class CartAddViewTest(TestCase):
+#     def setUp(self):
+#         self.factory = RequestFactory()
+#         # Create test category
+#         Category.objects.create(name="Test Category", slug="test-category")
+#         self.product = Product.objects.create(category_id=1, name="Test Product",slug="test-product", price=10.00)
+#         self.cart_session_id = settings.CART_SESSION_ID
+
+#     def test_cart_initialization(self):
+#         request = self.factory.get("/")
+#         cart = Cart(request)
+#         self.assertEqual(cart.cart, {})
